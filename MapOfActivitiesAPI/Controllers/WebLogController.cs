e2632113c3590c35461ebd403f2bec5983da8ca1
SergiyLapiuk <sergiy.lapiuk@knu.ua>
2024-11-30 02:02:51 +0200

    Добавьте файлы проекта.

---
+++ MapOfActivitiesAPI/Controllers/WebLogController.cs
@@ -0,0 +1,28 @@
+﻿using MapOfActivitiesAPI.Interfaces;
+using MapOfActivitiesAPI.Models;
+using MapOfActivitiesAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MapOfActivitiesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebLogController : Controller
+    {
+        private MapOfActivitiesAPIContext _context;
+
+        public WebLogController(MapOfActivitiesAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("all-web-logs")]
+        [Authorize(Roles = ApplicationUserRoles.Admin)]
+        public IEnumerable<WebLog> Get()
+        {
+            return _context.WebLogs.OrderByDescending(log => log.Time);
+        }
+    }
+}
