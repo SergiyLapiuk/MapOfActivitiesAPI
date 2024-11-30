cbc427109e723d23d780836faf5321eb0cda421b
SergiyLapiuk <sergiy.lapiuk@knu.ua>
2024-11-30 03:27:27 +0200

    fix program.cs

--- MapOfActivitiesAPI/Program.cs
+++ MapOfActivitiesAPI/Program.cs
@@ -75,15 +75,15 @@ if (!app.Environment.IsDevelopment())
 
 
 app.UseCors("AllowOrigin");
-if (/*app.Environment.IsDevelopment()*/)
+if (app.Environment.IsDevelopment())
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Map API V1");
-        c.RoutePrefix = string.Empty; // ������������� �� ������ ���� ������ ��������� swagger ��� ������� ������ ������� �������
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Map API V1");
+    c.RoutePrefix = string.Empty; // ������������� �� ������ ���� ������ ��������� swagger ��� ������� ������ ������� �������
+});
 
 
 if (app.Environment.IsDevelopment())
